0a4a59bacac4b0feed11691ea5876a9601bb7c8a
Aurelien Galicher <galicher@gmail.com>
2017-04-16 01:30:05 +0200

    *

---
+++ kafka-webapp-connector/src/stream-predict.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 12 18:09:33 2017
+
+@author: J0230022
+"""
+
+from __future__ import print_function
+
+from kafka import KafkaConsumer, KafkaProducer
+import requests
+from bson import json_util
+import json
+import sys
+
+KAFKA_BROKER = "kafka:9092"
+TOPIC = "mltest"
+GROUP_ID = "kafka_predictions"
+
+MODEL_NAME = "ridgecv"
+VERSION = 0
+
+PREDICTION_SERVICE = "http://prediction/predict/"
+
+def connect_kafka():
+    read_client = KafkaConsumer(TOPIC, group_id=GROUP_ID, bootstrap_servers=KAFKA_BROKER)
+    write_client = KafkaProducer(bootstrap_servers=KAFKA_BROKER)
+    return read_client, write_client
+
+def predict(msg, write_client, model_name, version):
+    if msg.key.decode('UTF-8') == 'features':
+        data = json.loads(msg.value.decode('UTF-8'))
+        timestamp = data['timestamp']
+        #feats = { key: data[key] for key in data.keys() if key.startswith('feat')}
+        connection_url = PREDICTION_SERVICE+model_name+'/'+str(version)+'/'
+        print (connection_url)
+        r = requests.post(connection_url, {'data': json.dumps([data], default=json_util.default)})
+        print (r.text)
+        prediction = json.loads(r.text)
+        for pred in prediction:
+            pred.update({'model':model_name,'version':version})
+            write_client.send(topic=TOPIC,value=json.dumps(pred).encode(encoding='UTF-8'),key=b'prediction')
+            print (pred)
+    return
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: stream_predict.py <topic> <model_name> <version>", file=sys.stderr)
+        sys.exit(-1)
+
+    TOPIC, MODEL_NAME, VERSION= sys.argv[1:]
+    VERSION= int(VERSION)
+
+    read_client, write_client = connect_kafka()
+    
+    for msg in read_client:
+        predict(msg, write_client, MODEL_NAME, VERSION)
---
+++ training-job/src/model_training.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+import pickle
+from pymongo import MongoClient
+import gridfs
+import pandas as pd
+from sklearn.linear_model import RidgeCV
+import sys
+import datetime 
+
+mongo = MongoClient("mongodb://mongo:27017",replicaset="rs0")
+fs = gridfs.GridFS(mongo.modeldb, collection="models")
+ts = mongo.mldb.ts
+
+def register_model(sk_model, model_name="", version=0, description="",score=None ):
+    pickle.dump(sk_model,open('save.p','wb'))
+    fs.put(open('save.p','rb'),filename='%s_v%s.p' %(model_name,version),model_name=model_name,version=version, description=description, score=score)
+    return
+
+def retrieve_training_dataset(ts, start, end):
+    data = [doc for doc in ts.find( {'time': {'$lt': end, '$gte': start}})]
+    df = pd.DataFrame(data).drop('_id', axis=1).drop('time', axis=1).set_index('timestamp')
+    return df
+
+def train_model(df):
+    y = df.target
+    X = df.drop('target',axis=1).fillna(0)
+    model = RidgeCV().fit(X,y)
+    score = model.score(X,y)
+    return model, score
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: model_traing.py <period in min> <model_name> <version>", file=sys.stderr)
+        sys.exit(-1)
+
+    period, model_name, version = sys.argv[1:]
+    period= int(period)
+    version = int(version)
+    end = datetime.datetime.now()
+    start = end - datetime.timedelta(minutes=period)
+    df = retrieve_training_dataset(ts, start, end).dropna()
+    model, score = train_model(df)
+    print ("r^2 score: %s" % score)
+    register_model(model, model_name=model_name, version=version, score=score, description="ridgeCV %s" % end.isoformat())
+
+    
